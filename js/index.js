e4c7d7c991053b3bad67026bd8d71f24ba7bd74e
JasonXChen <jasonxchen888@gmail.com>
2022-08-19 18:16:19 -0400

    Implement array of Civilian class that function as NPCs and spawn at random coordinates

--- js/index.js
+++ js/index.js
@@ -4,6 +4,7 @@ const ctx = canvas.getContext("2d");    // Apply 2D rendering context for canvas
 // Adjust canvas resolution based on screen resolution used
 canvas.setAttribute("height", getComputedStyle(canvas).height);
 canvas.setAttribute("width", getComputedStyle(canvas).width);
+const civArray = [];
 
 class Person    // Super class for all moving game entities
 {
@@ -21,8 +22,31 @@ class Person    // Super class for all moving game entities
         ctx.fillRect(this.x, this.y, this.width, this.height);
     }
 }
+class Civilian extends Person
+{
+    render = () =>
+    {
+        // Default height: 50px (5px hat 20px shirt, 20px pants, 5px shoes)
+        ctx.fillStyle = "white";
+        ctx.fillRect(this.x, this.y, this.width, 5); // Hat color
+        ctx.fillStyle = "red";
+        ctx.fillRect(this.x, this.y + 5, this.width, 20); // Shirt color
+        ctx.fillStyle = "blue";
+        ctx.fillRect(this.x, this.y + 25, this.width, 20); // Pants color
+        ctx.fillStyle = "black";
+        ctx.fillRect(this.x, this.y + 45, this.width, 5); // Shoe color
+    }
+}
 
 // FUNCTIONS
+const randomX = () =>    // Give random x-coordinate within canvas space
+{
+    return Math.floor(Math.random() * (canvas.width - 25));    // Max @ 25px left of end to allow space for object model
+}
+const randomY = () =>    // Give random y-coordinate within canvas space
+{
+    return Math.floor(Math.random() * (canvas.height - 50));    // Max @ 50px above end to allow space for object model
+}
 const playerInput = e =>
 {
     const step = 50;    // 1 pixel step at a time
@@ -76,8 +100,20 @@ const gameUpdate = () =>
     {
         player.render();
     }
+    civArray.forEach((civilian, i) =>
+    {
+        if (civilian.isAlive)
+        {
+            civilian.render();
+        }
+    })
 }
 
 // TEMP
+const player = new Person(500, 0, 25, 50);    // Initialization of Player
+for (let i = 0; i < 2; i++)
+{
+    const newCiv = new Civilian(randomX(), randomY(), 25, 50);
+    civArray.push(newCiv);
+}
 const gameUpdateInterval = setInterval(gameUpdate, 100);    // USE FOR REFRESHING SCREEN EVERY 60 ms
-const player = new Person(500, 0, 25, 50);
