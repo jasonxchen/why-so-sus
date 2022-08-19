ff2e706b2006273d4e36401c7f50223d76294aad
JasonXChen <jasonxchen888@gmail.com>
2022-08-19 17:05:53 -0400

    Add HTML, CSS, and JS files to display minimum product with player movement

---
+++ js/index.js
@@ -0,0 +1,83 @@
+// VARIABLES
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");    // Apply 2D rendering context for canvas
+// Adjust canvas resolution based on screen resolution used
+canvas.setAttribute("height", getComputedStyle(canvas).height);
+canvas.setAttribute("width", getComputedStyle(canvas).width);
+
+class Person    // Super class for all moving game entities
+{
+    constructor(xPos, yPos, width, height)
+    {
+        this.x = xPos;
+        this.y = yPos;
+        this.width = width;
+        this.height = height;
+        this.isAlive = true;
+    }
+    render = () =>
+    {
+        ctx.fillStyle = "black";    // Default black render
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+    }
+}
+
+// FUNCTIONS
+const playerInput = e =>
+{
+    const step = 50;    // 1 pixel step at a time
+    if (player.isAlive)
+    {
+        switch (e.key)
+        {
+            case "w":
+                player.y -= step;    // Move player up
+                if (player.y < 0)
+                {
+                    player.y = 0;    // Prevent moving out of top of screen
+                }
+                break;
+            case "a":
+                player.x -= step;    // Move player left
+                if (player.x < 0)
+                {
+                    player.x = 0;    // Prevent moving out of left of screen
+                }
+                break;
+            case "s":
+                player.y += step;    // Move player down
+                if (player.y + player.height > canvas.height)
+                {
+                    player.y = canvas.height - player.height;        // Prevent moving out of bottom of screen
+                }
+                break;
+            case "d":
+                player.x += step;    // Move player right
+                if (player.x + player.width > canvas.width)
+                {
+                    player.x = canvas.width - player.width;        // Prevent moving out of right of screen
+                }
+                break;
+//          case " ":
+//              console.log("pew");
+            default:
+                break;
+        }
+//        gameUpdate();    // USE FOR REFRESHING SCREEN ON MOVEMENT
+    }
+}
+document.addEventListener("keydown", playerInput);
+
+const gameUpdate = () =>
+{
+    ctx.clearRect(0, 0, canvas.width, canvas.height);    // Clear the canvas
+    // Rendering
+    if (player.isAlive)
+    {
+        player.render();
+    }
+}
+
+// TEMP
+const gameUpdateInterval = setInterval(gameUpdate, 100);    // USE FOR REFRESHING SCREEN EVERY 60 ms
+const player = new Person(500, 0, 25, 50);
